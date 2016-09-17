7e4863f33f532751c5d3c389c0bdd4fb5e4d0407
Christoph Hegemann <christoph.hegemann1337@gmail.com>
2016-09-17 09:23:11 +0200

    whitespace

--- init.el
+++ init.el
@@ -1,14 +1,14 @@
 (setq debug-on-error t)
-(setq delete-old-versions -1 )		; delete excess backup versions silently
-(setq version-control t )		; use version control
-(setq vc-make-backup-files t )		; make backups file even when in version controlled dir
+(setq delete-old-versions -1 ); delete excess backup versions silently
+(setq version-control t ); use version control
+(setq vc-make-backup-files t ); make backups file even when in version controlled dir
 (setq backup-directory-alist `(("." . "~/.emacs.d/backups")) ) ; which directory to put backups file
-(setq vc-follow-symlinks t )				       ; don't ask for confirmation when opening symlinked file
+(setq vc-follow-symlinks t ); don't ask for confirmation when opening symlinked file
 (setq auto-save-file-name-transforms '((".*" "~/.emacs.d/auto-save-list/" t)) ) ;transform backups file name
-(setq inhibit-startup-screen t )	; inhibit useless and old-school startup screen
+(setq inhibit-startup-screen t ); inhibit useless and old-school startup screen
 (setq coding-system-for-write 'utf-8 )
-(setq sentence-end-double-space nil)	; sentence SHOULD end with only a point.
-(setq default-fill-column 80)		; toggle wrapping text at the 80th character
+(setq sentence-end-double-space nil); sentence SHOULD end with only a point.
+(setq default-fill-column 80); toggle wrapping text at the 80th character
 (setq initial-scratch-message "Welcome in Emacs") ; print a default message in the empty scratch buffer opened at startupa
 (scroll-bar-mode 0)
 (menu-bar-mode 0)
@@ -185,7 +185,7 @@
 ;; theme
 (use-package color-theme-sanityinc-tomorrow
   :ensure t
-  :config 
+  :config
   (load-theme 'sanityinc-tomorrow-eighties t)
   (set-face-attribute 'default nil :family "PragmataPro")
   (set-face-attribute 'default nil :height 130))
@@ -289,5 +289,10 @@
 
 (use-package org-ref
   :ensure t)
+
+(use-package ethan-wspace
+  :ensure t
+  :init (setq mode-require-final-newline nil)
+  :config (global-ethan-wspace-mode 1))
 
 (setq debug-on-error nil)
