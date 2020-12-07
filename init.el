74f9840e233727044f6084b14c675287a3d79cf9
kritzcreek <christoph.hegemann1337@gmail.com>
2020-12-07 15:34:20 +0100

    Emacs's built in fill indicator uses a different variable

--- init.el
+++ init.el
@@ -8,7 +8,7 @@
 (setq inhibit-startup-screen t ); inhibit useless and old-school startup screen
 (setq coding-system-for-write 'utf-8 )
 (setq sentence-end-double-space nil); sentence SHOULD end with only a point.
-(setq default-fill-column 80); toggle wrapping text at the 80th character
+(setq fill-column 80); toggle wrapping text at the 80th character
 (setq initial-scratch-message "Welcome in Emacs") ; print a default message in the empty scratch buffer opened at startupa
 (scroll-bar-mode 0)
 (menu-bar-mode 0)
