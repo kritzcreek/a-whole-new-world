4410b70528d731bccb3e0b833d717b1eff57a1e9
Christoph <christoph.hegemann@sourcegraph.com>
2024-01-04 10:00:20 +0100

    formatting

--- init.el
+++ init.el
@@ -1,12 +1,12 @@
 (setq debug-on-error t)
-(setq delete-old-versions -1 ); delete excess backup versions silently
-(setq version-control t ); use version control
-(setq vc-make-backup-files t ); make backups file even when in version controlled dir
-(setq backup-directory-alist `(("." . "~/.emacs.d/backups")) ) ; which directory to put backups file
-(setq vc-follow-symlinks t ); don't ask for confirmation when opening symlinked file
-(setq auto-save-file-name-transforms '((".*" "~/.emacs.d/auto-save-list/" t)) ) ;transform backups file name
-(setq inhibit-startup-screen t ); inhibit useless and old-school startup screen
-(setq coding-system-for-write 'utf-8 )
+(setq delete-old-versions -1); delete excess backup versions silently
+(setq version-control t); use version control
+(setq vc-make-backup-files t); make backups file even when in version controlled dir
+(setq backup-directory-alist `(("." . "~/.emacs.d/backups"))) ; which directory to put backups file
+(setq vc-follow-symlinks t); don't ask for confirmation when opening symlinked file
+(setq auto-save-file-name-transforms '((".*" "~/.emacs.d/auto-save-list/" t))) ;transform backups file name
+(setq inhibit-startup-screen t); inhibit useless and old-school startup screen
+(setq coding-system-for-write 'utf-8)
 (setq sentence-end-double-space nil); sentence SHOULD end with only a point.
 (setq fill-column 100); toggle wrapping text at the 90th character
 (setq initial-scratch-message "Cheers love, the cavalry's here!") ; print a default message in the empty scratch buffer opened at startup
